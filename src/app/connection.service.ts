c0e9f8db90de86aea7d0beaa0ef62a5ad61a4d06
salni84 <roberto.salerno@swissscom.com>
2020-12-27 18:12:42 +0100

    deployment

--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {WebSocketSubject} from "rxjs/internal-compatibility";
-import {webSocket} from "rxjs/webSocket";
+import {WebSocketSubject} from 'rxjs/internal-compatibility';
+import {webSocket} from 'rxjs/webSocket';
 
 @Injectable({
   providedIn: 'root'
